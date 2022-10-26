3811dcbd40a74a2d6a5ab225886a9c40b6defa1e
Piyushnmj <piyushnmj@yahoo.com>
2022-10-26 11:13:57 +0530

    Contact Details

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace AddressBook
+﻿using AddressBook.Model;
+using AddressBook.Repository;
+
+namespace AddressBook
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book");
+            ContactDetailsRepository contactDetails = new ContactDetailsRepository();
+            contactDetails.AddContactDetails("Virat", "Kohli", "9009009321", "viratkohli@gmail.com", "Worli", "Mumbai", "Maharashtra", "400018");
+            contactDetails.DisplayContact();
+            Console.ReadLine();
         }
     }
+
 }
+
---
+++ AddressBook/AddressBook/Repository/ContactDetailsRepository.cs
@@ -0,0 +1,43 @@
+﻿using AddressBook.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook.Repository
+{
+    internal class ContactDetailsRepository
+    {
+        Dictionary<string, ContactDetails> contactDetails = new Dictionary<string, ContactDetails>();
+
+        public void AddContactDetails(string firstName, string lastName, string mobileNumber, string eMail, string address, string city, string state, string zip)
+        {
+            ContactDetails contactDetail = new ContactDetails();
+            contactDetail.FirstName = firstName;
+            contactDetail.LastName = lastName;
+            contactDetail.MobileNumber = Convert.ToInt64(mobileNumber);
+            contactDetail.EMail = eMail;
+            contactDetail.Address = address;
+            contactDetail.City = city;
+            contactDetail.State = state;
+            contactDetail.Zip = Convert.ToInt32(zip);
+            contactDetails.Add(Convert.ToString(contactDetail.MobileNumber), contactDetail);
+        }
+
+        public void DisplayContact()
+        {
+            foreach (var item in contactDetails)
+            {
+                Console.WriteLine($"\nFirst Name::{item.Value.FirstName}");
+                Console.WriteLine($"Last Name::{item.Value.LastName}");
+                Console.WriteLine($"Mobile Number::{item.Value.MobileNumber}");
+                Console.WriteLine($"EMail::{item.Value.EMail}");
+                Console.WriteLine($"Address::{item.Value.Address}");
+                Console.WriteLine($"City::{item.Value.City}");
+                Console.WriteLine($"State::{item.Value.State}");
+                Console.WriteLine($"Zip::{item.Value.Zip}");
+            }
+        }
+    }
+}
