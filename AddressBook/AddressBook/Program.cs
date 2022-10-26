808caa4cf9a830c4aadc80881cbaf9700a6a6681
Piyushnmj <piyushnmj@yahoo.com>
2022-10-26 14:47:20 +0530

    Contact Details

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -7,8 +7,7 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book");           
-
+            Console.WriteLine("Welcome to Address Book");
             ContactDetailsRepository contactDetails = new ContactDetailsRepository();
             contactDetails.AddContactDetails("Virat", "Kohli", 9009009321, "viratkohli@gmail.com", "Worli", "Mumbai", "Maharashtra", 400018);
             contactDetails.DisplayContact();
